01400663428c3106308948485e96bfa209f00491
Chetan Agarwal <chetan.mouselangelo@gmail.com>
2020-06-03 05:08:28 +0200

    Setup electron config + app info + libs

---
+++ electron/webpack.config.js
@@ -0,0 +1,12 @@
+const { withExpoWebpack } = require("@expo/electron-adapter");
+
+module.exports = (config) => {
+  const expoConfig = withExpoWebpack(config);
+  expoConfig.node = {
+    ...expoConfig.node,
+    __dirname: false,
+    __filename: false,
+  };
+  console.log("expo config", expoConfig);
+  return expoConfig;
+};
