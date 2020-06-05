5d1ffdb2111a39592eb1ddc8e367c096a4a62d7a
Chetan Agarwal <chetan.mouselangelo@gmail.com>
2020-06-05 05:54:47 +0200

    Add 'Generate subtitles' button

--- app/App.tsx
+++ app/App.tsx
@@ -10,17 +10,33 @@ const App = () => {
     "/Users/chetan/Documents/videos/clips/hello.mp4"
   );
 
-  const importVideoAction: NavigationAction = {
-    title: "Import video",
-    action: async () => {
-      const file = await importVideo();
-      setVideoSrc(file);
-    },
-  };
+  const [subtitles, setSubtitles] = useState<object | null>(null);
+
+  const actions = [] as NavigationAction[];
+
+  if (!videoSrc) {
+    actions.push({
+      title: "Import video",
+      action: async () => {
+        const file = await importVideo();
+        setVideoSrc(file);
+      },
+    });
+  }
+
+  if (videoSrc && !subtitles) {
+    actions.push({
+      title: "Generate Subtitles",
+      action: async () => {
+        //
+      },
+    });
+  }
+
   info();
   return (
     <View style={styles.container}>
-      <NavigationBar actions={[importVideoAction]} />
+      <NavigationBar actions={actions} />
       {videoSrc && <VideoPlayer file={videoSrc} />}
     </View>
   );
