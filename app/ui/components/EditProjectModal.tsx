16237c59f43d3e9d903ab48626e96a49658aaeee
Chetan Agarwal <chetan.mouselangelo@gmail.com>
2020-06-18 07:44:12 +0200

    Cleanup UI

--- app/ui/components/EditProjectModal.tsx
+++ app/ui/components/EditProjectModal.tsx
@@ -33,104 +33,104 @@ const EditProjectModal: React.FC<{ project: Project }> = ({ project }) => {
   return (
     <Provider theme={theme}>
       <Portal>
-        <Modal visible={true} dismissable={false}>
+        <Modal
+          visible={true}
+          dismissable={false}
+          contentContainerStyle={styles.modal}
+        >
           <View style={styles.container}>
             <Appbar.Header style={styles.header}>
               <Appbar.Content title="Project Details" />
             </Appbar.Header>
-            <View style={styles.content}>
-              <Card style={styles.card}>
-                <View style={styles.videoContainer}>
-                  <VideoPreview file={videoFile} height={320} />
-                </View>
-                <Card.Content>
-                  <TextInput
-                    label="Title"
-                    placeholder="I can see ..."
-                    value={projectTitle}
-                    onChangeText={(text) => {
-                      setProjectTitle(text);
-                      setDidEdit(true);
-                      setDidSave(false);
-                    }}
-                    style={styles.input}
-                  />
-                  <TextInput
-                    label="Description (optional)"
-                    placeholder="It's gonna be a bright, sun-shiny day..."
-                    multiline={true}
-                    numberOfLines={3}
-                    value={description}
-                    onChangeText={(text) => {
-                      setDescription(text);
-                      setDidEdit(true);
-                    }}
-                    style={styles.input}
-                  />
-                </Card.Content>
-                <Card.Actions
-                  style={[
-                    styles.actions,
-                    {
-                      justifyContent: showDiscard
-                        ? "space-between"
-                        : "flex-end",
-                    },
-                  ]}
-                >
-                  {showDiscard && (
-                    <Button
-                      theme={{ colors: { primary: theme.colors.negative } }}
-                      onPress={() => {
-                        if (didEdit) {
-                          // show alert
-                          console.log("discard alert");
-                        } else {
-                          console.log("dismiss modal");
-                        }
-                      }}
-                    >
-                      Discard
-                    </Button>
-                  )}
+            <Card style={styles.card}>
+              <View style={styles.videoContainer}>
+                <VideoPreview file={videoFile} height={320} />
+              </View>
+              <Card.Content>
+                <TextInput
+                  label="Title"
+                  placeholder="I can see ..."
+                  value={projectTitle}
+                  onChangeText={(text) => {
+                    setProjectTitle(text);
+                    setDidEdit(true);
+                    setDidSave(false);
+                  }}
+                  style={styles.input}
+                />
+                <TextInput
+                  label="Description (optional)"
+                  placeholder="It's gonna be a bright, sun-shiny day..."
+                  multiline={true}
+                  numberOfLines={3}
+                  value={description}
+                  onChangeText={(text) => {
+                    setDescription(text);
+                    setDidEdit(true);
+                  }}
+                  style={styles.input}
+                />
+              </Card.Content>
+              <Card.Actions
+                style={[
+                  styles.actions,
+                  {
+                    justifyContent: showDiscard ? "space-between" : "flex-end",
+                  },
+                ]}
+              >
+                {showDiscard && (
                   <Button
-                    theme={{ colors: { primary: theme.colors.positive } }}
-                    onPress={async () => {
-                      if (!projectCreated) {
-                        setIsCreating(true);
-                        const project = {
-                          videoFile,
-                          title: projectTitle,
-                          description,
-                        };
-                        const projectFile = await saveProject({
-                          project,
-                        });
-                        setIsCreating(false);
-                        setProjectFile(projectFile);
-                        if (projectFile) {
-                          setDidSave(true);
-                        }
+                    theme={{ colors: { primary: theme.colors.negative } }}
+                    onPress={() => {
+                      if (didEdit) {
+                        // show alert
+                        console.log("discard alert");
                       } else {
-                        if (didEdit) {
-                          console.log("save to", projectFile);
-                        } else {
-                          console.log("done");
-                        }
+                        console.log("dismiss modal");
                       }
                     }}
                   >
-                    {projectCreated
-                      ? didEdit
-                        ? "Save"
-                        : "Done"
-                      : i18n.t("projects.create")}
+                    Discard
                   </Button>
-                </Card.Actions>
-                {isCreating && <Loader isLoading={isCreating} />}
-              </Card>
-            </View>
+                )}
+                <Button
+                  theme={{ colors: { primary: theme.colors.positive } }}
+                  onPress={async () => {
+                    if (!projectCreated) {
+                      setIsCreating(true);
+                      const project = {
+                        videoFile,
+                        title: projectTitle,
+                        description,
+                      };
+                      const projectFile = await saveProject({
+                        project,
+                      });
+                      setIsCreating(false);
+                      setProjectFile(projectFile);
+                      if (projectFile) {
+                        setDidSave(true);
+                      }
+                    } else {
+                      if (didEdit) {
+                        console.log("save to", projectFile);
+                      } else {
+                        console.log("done");
+                      }
+                    }
+                  }}
+                >
+                  {projectCreated
+                    ? didEdit
+                      ? "Save"
+                      : "Done"
+                    : i18n.t("projects.create")}
+                </Button>
+              </Card.Actions>
+            </Card>
           </View>
+          {isCreating && <Loader isLoading={isCreating} />}
         </Modal>
       </Portal>
     </Provider>
@@ -138,22 +138,21 @@ const EditProjectModal: React.FC<{ project: Project }> = ({ project }) => {
 };
 
 const styles = StyleSheet.create({
+  modal: {
+    marginBottom: 60,
+  },
   container: {
     alignItems: "center",
   },
-  content: {
-    backgroundColor: theme.colors.background,
-    borderBottomLeftRadius: 8,
-    borderBottomRightRadius: 8,
-    padding: 1,
-  },
   header: {
-    width: 602,
+    width: 600,
     height: 48,
   },
   card: {
     width: 600,
     minHeight: 600,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
   },
   videoContainer: {
     margin: 16,
