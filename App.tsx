6d9c849400c780c8d40f4d45bda27741d563c107
Chetan Agarwal <chetan.mouselangelo@gmail.com>
2020-06-18 06:25:08 +0200

    Don't use TestApp

--- App.tsx
+++ App.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import { AppRegistry } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import { expo } from "./app.json";
-import TestApp from "./app/TestApp";
 import App from "./app/App";
 import { NavigationContainer } from "@react-navigation/native";
 import { theme } from "./app/ui/style/theme";
 
-const isTesting = false;
-
 const MainApp = () => {
-  return isTesting ? <TestApp /> : <App />;
+  return <App />;
 };
 
 export default function Main() {
